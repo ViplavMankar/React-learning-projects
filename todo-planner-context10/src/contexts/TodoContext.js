a51887894b43f7dfce0248ea3b3ac6dd506a3c50
ViplavMankar <viplavmankar@gmail.com>
2025-01-23 22:18:50 +0530

    After Todo Planner application

---
+++ todo-planner-context10/src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todoTitle: "Todo Message",
+            completed: false
+        }
+    ],
+    addTodo: (todoTitle) => {},
+    updateTodo: (id, todoTitle) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider;
